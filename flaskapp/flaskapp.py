e0fb24a87c89f0d9d97f153f8a7c4dbcd56269ec
Beenjamming <michaebs@mail.uc.edu>
2024-04-07 22:51:48 -0400

    updates to flask app

--- flaskapp/flaskapp.py
+++ flaskapp/flaskapp.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
+import pandas as pd
+import os
+
 app = Flask(__name__)
 
-@app.route("/")
-def flaskapp():
-    file="about9.jpg"
-    return render_template("flaskapp.html",file=file)
+@app.route("/styles.css")
+def styles():
+    return url_for('static', filename='styles.css')
+
+@app.route('/')
+def home():
+    file=url_for('static', filename='about9.jpg')
+    return render_template('flaskapp.html', file=file)
 
 if __name__ == "__main__":
     app.run()
